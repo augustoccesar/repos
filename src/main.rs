cc6b8844b5f5d1fc6509987d85c6e6802ecc945a
Augusto César <augusto.silva@mentimeter.com>
2024-04-22 08:14:50 +0200

    feat: add clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use clap::{command, Parser};
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// Repository name
+    #[arg()]
+    name: String,
+
+    #[arg(short, long)]
+    verbose: bool
+}
+
 fn main() {
-    println!("Hello, world!");
+    // github.com/augustoccesar/adventofcode    -- default ssh
+    // augustoccesar/adventofcode               -- default github.com (overwrittable by config)
+    // adventofcode                             -- default github.com + whoami (overwrittable by config)
+    // aoc                                      -- configurable shortcut
+    let args = Args::parse();
+
+    println!("{}", args.name);
+    println!("{}", args.verbose);
 }
