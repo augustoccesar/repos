3f6d6515ca614aa9a2c3c11943b45ad9a716644e
Augusto César <augusto.silva@mentimeter.com>
2024-04-23 08:22:03 +0200

    feat: add config for alias to full name

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 use std::{
-    fs::OpenOptions,
+    collections::HashMap,
+    fs::{self, File, OpenOptions},
     io::{Read, Seek, Write},
     path::Path,
     process::exit,
 };
 
 use clap::{command, Args, Parser, Subcommand};
+use serde::{Deserialize, Serialize};
 
 #[derive(Parser)]
 struct Cli {
@@ -46,7 +48,7 @@ enum RepoName {
 }
 
 impl RepoName {
-    pub fn local_path(&self) -> String {
+    pub fn local_path(&self, config: &Config) -> String {
         match self {
             RepoName::Full(repo_name) => {
                 format!(
@@ -58,25 +60,41 @@ impl RepoName {
                 )
             }
             RepoName::UserRepo(_, _) => todo!(),
-            RepoName::RepoOnly(_) => todo!(),
+            RepoName::RepoOnly(repo_name) => {
+                if let Some(aliases) = &config.aliases {
+                    if let Some(alias) = aliases.get(repo_name) {
+                        if alias == repo_name {
+                            panic!("Infinite loop");
+                        }
+
+                        // TODO: Handle error
+                        let alias_repo_name = RepoName::try_from(alias).unwrap();
+
+                        return alias_repo_name.local_path(config);
+                    }
+                }
+
+                todo!()
+            }
         }
     }
 
+    #[allow(dead_code)]
     pub fn clone_url(&self) -> String {
         match self {
             RepoName::Full(info) => {
                 format!("git@{}:{}/{}.git", info.host, info.username, info.repo)
-            },
+            }
             RepoName::UserRepo(_, _) => todo!(),
             RepoName::RepoOnly(_) => todo!(),
         }
     }
 }
 
-impl TryFrom<String> for RepoName {
+impl TryFrom<&String> for RepoName {
     type Error = String; // TODO: Better error type
 
-    fn try_from(value: String) -> Result<Self, Self::Error> {
+    fn try_from(value: &String) -> Result<Self, Self::Error> {
         let parts: Vec<&str> = value.split('/').collect();
 
         match parts.len() {
@@ -101,20 +119,55 @@ impl TryFrom<String> for RepoName {
     }
 }
 
+// TODO: Create subcommand to manage this config
+#[derive(Serialize, Deserialize)]
+struct Config {
+    aliases: Option<HashMap<String, String>>,
+}
+
+impl Config {
+    pub fn load() -> Self {
+        let folder_path = repos_folder_path();
+        let path = format!("{}/.config.json", &folder_path);
+        let path = Path::new(&path);
+
+        let data = if path.exists() {
+            fs::read_to_string(path).unwrap()
+        } else {
+            fs::create_dir_all(&folder_path).unwrap();
+
+            let data = "{\n}";
+            let mut file = File::create(path).unwrap();
+            file.write_all(data.as_bytes()).unwrap();
+
+            data.to_string()
+        };
+
+        serde_json::from_str(&data).unwrap()
+    }
+}
+
+// TODO: Maybe make this into a lazy static?
+fn repos_folder_path() -> String {
+    let home_path = dirs::home_dir().unwrap().to_str().unwrap().to_string();
+    format!("{}/repos", home_path)
+}
+
 fn main() {
     //   - github.com/augustoccesar/adventofcode    -- default ssh
     //   - augustoccesar/adventofcode               -- default github.com (overwrittable by config)
     //   - adventofcode                             -- default github.com + whoami (overwrittable by config)
     //   - aoc                                      -- configurable shortcut
     let cli = Cli::parse();
+    let config = Config::load();
 
     match cli.command {
         Command::Expand(args) => {
-            let repo_name = RepoName::try_from(args.name);
+            let repo_name = RepoName::try_from(&args.name);
 
             match repo_name {
                 Ok(repo_name) => {
-                    let path = repo_name.local_path();
+                    let path = repo_name.local_path(&config);
                     let exists = Path::new(&path).exists();
 
                     if exists {
