4cb4d83e7aa8a4d54295a0ad3874a0e0785f97fb
Augusto César <augusto@augustocesar.se>
2024-04-23 21:34:32 +0200

    feat: add clone if repo not found locally

--- src/main.rs
+++ src/main.rs
@@ -223,6 +223,23 @@ fn ensure_repos_folder_exists() {
     fs::create_dir_all(repos_folder_path()).unwrap();
 }
 
+// TODO: Can this dependency on Config be removed?
+fn clone_repo(repo_name: &RepoName, config: &Config) {
+    let clone_url = repo_name.clone_url();
+
+    let output = std::process::Command::new("git")
+        .args(["clone", &clone_url, &repo_name.local_path(config)])
+        .output()
+        .expect("failed to execute process");
+
+    if !output.status.success() {
+        println!(
+            "failed to clone repo: {}",
+            String::from_utf8_lossy(output.stderr.as_slice())
+        );
+    }
+}
+
 fn main() {
     //   - git@github.com:augustoccesar/adventofcode.git    -- as is. /git@(.+):(.+)\/(.+)\.git/
     //   - github.com/augustoccesar/adventofcode            -- default ssh
@@ -243,12 +260,12 @@ fn main() {
 
                     if exists {
                         print!("{}", &path);
-                        exit(0);
                     } else {
-                        // TODO: clone if does not exist
-                        print!("Could not find repository locally. Does not support clone yet.");
-                        exit(1);
+                        clone_repo(&repo_name, &config);
+                        print!("{}", &path);
                     }
+
+                    exit(0);
                 }
                 Err(_) => todo!(),
             }
