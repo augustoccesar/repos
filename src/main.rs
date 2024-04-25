ff36643a18b0eef3aaa145dfcb2e6be421b01653
Augusto César <augusto.silva@mentimeter.com>
2024-04-25 08:30:50 +0200

    feat: add confirmation on setup command

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@
 use std::{
     collections::HashMap,
     fs::{self, File, OpenOptions},
-    io::{Read, Seek, Write},
+    io::{self, Read, Seek, Write},
     path::Path,
     process::exit,
     sync::OnceLock,
@@ -316,10 +316,30 @@ fn main() {
             }
         }
         Command::Setup(_) => {
-            let function = include_str!("../shell_setup");
+            let shell_setup = include_str!("../shell_setup");
+            let rc_file_name = ".zshrc";
+
+            let mut confirmation = String::new();
+            println!(
+                "Setup will add the following to to your {} file",
+                rc_file_name
+            );
+            println!("```");
+            println!("{}", shell_setup);
+            println!("```");
+            println!("Do you want to continue? (y/n - only 'y' continue)");
+            match io::stdin().read_line(&mut confirmation) {
+                Ok(_) => {
+                    if confirmation.trim() != "y" {
+                        println!("Aborted!");
+                        exit(0);
+                    }
+                }
+                Err(_) => exit(1),
+            }
 
             // TODO: Support files other than zsh
-            let rc_file_path = format!("{}/.zshrc", home_path());
+            let rc_file_path = format!("{}/{}", home_path(), rc_file_name);
 
             let mut rc_file = OpenOptions::new()
                 .read(true)
@@ -337,10 +357,10 @@ fn main() {
             }
 
             rc_file.rewind().unwrap();
-            rc_file.write_all(function.as_bytes()).unwrap();
+            rc_file.write_all(shell_setup.as_bytes()).unwrap();
 
             println!("Ready!");
-            println!("Run 'source ~/.zshrc' to reflect changes.");
+            println!("Run source your '{}' to reflect changes.", rc_file_name);
             exit(0);
         }
         Command::Config(args) => {
