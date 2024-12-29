482d10ec1a2538c51594b5b655c7fe8d41481030
Augusto Cesar <augusto@augustocesar.se>
2024-12-29 18:11:17 +0100

    refactor(list): change how folders are printed

--- src/commands/list.rs
+++ src/commands/list.rs
@@ -9,32 +9,103 @@ use crate::{
 pub struct Args {}
 
 pub fn list(_args: &Args, config: &Config) -> Result<()> {
-    print_folder(&PathBuf::from(&repos_folder_path()), config, 0);
+    let root = Folder::root().visit(config);
+
+    print_folder(&root, "", true);
 
     Ok(())
 }
 
-pub fn print_folder(path: &PathBuf, config: &Config, level: u16) {
-    let paths = fs::read_dir(path).unwrap();
-    for item in paths.flatten() {
-        if item.metadata().unwrap().is_dir() {
-            let folder_name = item.file_name().into_string().unwrap();
-            print!("{}{}", "| ".repeat(level as usize), folder_name);
+fn print_folder(folder: &Folder, prefix: &str, is_last: bool) {
+    let folder_name = folder
+        .path
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        .to_string();
 
-            if let Some(aliases) = &config.aliases {
-                if let Some(entry) = aliases
-                    .iter()
-                    .find(|&(_, v)| *v == item.path().display().to_string())
-                {
-                    print!(" (alias: {})", entry.0);
-                }
-            }
+    if folder.is_root {
+        print!("{}", folder_name);
+    } else if is_last {
+        print!("{}└ {}", prefix, folder_name);
+    } else {
+        print!("{}├ {}", prefix, folder_name);
+    }
 
-            println!();
+    if let (true, Some(alias)) = (folder.is_repo, &folder.alias) {
+        print!(" (alias: {})", &alias);
+    }
 
-            if !item.path().join(".git").exists() {
-                print_folder(&item.path(), config, level + 1);
+    println!();
+
+    for (i, subfolder) in folder.sub_folders.iter().enumerate() {
+        let mut new_prefix = String::from(prefix);
+
+        if !folder.is_root {
+            if is_last {
+                new_prefix.push_str("  ");
+            } else {
+                new_prefix.push_str("│ ");
             }
         }
+
+        let is_last_subfolder = i == folder.sub_folders.len() - 1;
+        print_folder(subfolder, &new_prefix, is_last_subfolder);
+    }
+}
+
+#[derive(Debug, Default)]
+struct Folder {
+    path: PathBuf,
+    is_root: bool,
+    is_repo: bool,
+    alias: Option<String>,
+    sub_folders: Vec<Folder>,
+}
+
+impl Folder {
+    fn new(path: PathBuf, is_repo: bool, alias: Option<String>) -> Self {
+        Self {
+            path,
+            is_repo,
+            alias,
+            ..Default::default()
+        }
+    }
+
+    fn root() -> Self {
+        Self {
+            path: PathBuf::from(&repos_folder_path()),
+            is_root: true,
+            ..Default::default()
+        }
+    }
+
+    fn visit(mut self, config: &Config) -> Self {
+        let paths = fs::read_dir(&self.path).unwrap();
+        for item in paths.flatten() {
+            if item.metadata().unwrap().is_dir() {
+                let alias = if let Some(aliases) = &config.aliases {
+                    aliases
+                        .iter()
+                        .find(|&(_, v)| *v == item.path().display().to_string())
+                        .map(|kv| kv.0.to_owned())
+                } else {
+                    None
+                };
+
+                let is_repo = item.path().join(".git").exists();
+
+                let mut folder = Folder::new(item.path(), is_repo, alias);
+                if !folder.is_repo {
+                    folder = folder.visit(config);
+                }
+
+                self.sub_folders.push(folder);
+            }
+        }
+
+        self
     }
 }
