a3d3f20e8e452160670b9d09024a4517a33f6c54
Augusto Cesar <augusto@augustocesar.se>
2025-01-11 15:09:57 +0100

    feat: wip version of expand command

--- app/src/main/java/se/augustocesar/repos/Repos.java
+++ app/src/main/java/se/augustocesar/repos/Repos.java
@@ -1,7 +1,16 @@
 package se.augustocesar.repos;
 
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import se.augustocesar.repos.commands.Expand;
+
+@Command(name = "repos", mixinStandardHelpOptions = true)
 public class Repos {
     public static void main(String[] args) {
-        System.out.println("Hello!");
+        var commandLine = new CommandLine(new Repos());
+        commandLine.addSubcommand("expand", new Expand());
+
+        var exitCode = commandLine.execute(args);
+        System.exit(exitCode);
     }
 }
---
+++ app/src/main/java/se/augustocesar/repos/commands/Expand.java
@@ -0,0 +1,40 @@
+package se.augustocesar.repos.commands;
+
+import java.util.concurrent.Callable;
+
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Parameters;
+import se.augustocesar.repos.RepositoryInfo;
+
+@Command(name = "expand", mixinStandardHelpOptions = true, description = """
+        This command expands the passed on repository name to the full path.
+
+        For cases where the fields are not all present on the name, they will be resolved by:
+
+            host:
+                1. What is on the `default_host` of the config.
+                2. Default to "github.com".
+
+            username:
+                1. What is on the `default_username` of the config.
+                2. Default to whoami::username()
+
+            Supported formats:
+                - git@{host}:{username}/{repo}.git
+                - {host}/{username}/{repo}
+                - {username}/{repo}
+                - {repo}
+        """)
+public class Expand implements Callable<Integer> {
+    @Parameters(index = "0", description = "repository name on one of the supported formats")
+    String name;
+
+    @Override
+    public Integer call() {
+        System.out.println("Expanding " + this.name);
+
+        System.out.println(RepositoryInfo.of(this.name));
+
+        return 0;
+    }
+}
