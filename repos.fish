d7dc5f3e69e60253d87c799435903fb294e62a80
Augusto César <augusto@augustocesar.se>
2025-04-28 08:37:58 +0200

    feat: split fish shell files

--- repos.fish
+++ repos.fish
@@ -1,18 +1,7 @@
 function rcd
-    # Gradle Testing
-    set REPO (./gradlew -q run --args="expand $argv[1] --clone" --rerun-tasks | tee /dev/tty)
-
-    # Compiled Testing
-    # set REPO (./app/build/native/nativeCompile/repos expand $argv[1] --clone | tee /dev/tty)
-
-    # Live
-    # set REPO (repos expand $argv[1] --clone | tee /dev/tty)
+    set REPO (repos expand $argv[1] --clone | tee /dev/tty)
 
     if test $pipestatus[1] -eq 0
-        # Testing
-        echo $REPO
-
-        # Live
-        # cd $REPO
+        cd $REPO
     end
 end
