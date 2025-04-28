5a99e5692b2e6158bda9eeafff67c57927d78115
Augusto César <augusto@augustocesar.se>
2025-04-28 08:41:34 +0200

    refactor: move tree display to command

--- app/src/main/java/se/augustocesar/repos/ReposDir.java
+++ app/src/main/java/se/augustocesar/repos/ReposDir.java
@@ -32,31 +32,7 @@ public class ReposDir {
         ).collect(Collectors.toList());
     }
 
-    public String displayTree(String filter) {
-        var filteredRepos = this.list(filter).stream()
-                .map(repo -> {
-                    String path = pathFromBase(repo);
-
-                    return path.startsWith("/") ? path.substring(1) : path;
-                })
-                .sorted()
-                .toList();
-
-        Node root = new Node(-1, "root");
-        for (String repoPath : filteredRepos) {
-            root.add(repoPath);
-        }
-
-        StringBuilder tree = new StringBuilder();
-        for (int i = 0; i < root.leaves.size(); i++) {
-            boolean isLast = i == root.leaves.size() - 1;
-            root.leaves.get(i).asTree(tree, "", isLast);
-        }
-
-        return tree.toString();
-    }
-
-    private String pathFromBase(String repoFullPath) {
+    public static String pathFromBase(String repoFullPath) {
         return repoFullPath.startsWith(Constants.REPOS_DIR_PATH)
                 ? repoFullPath.substring(Constants.REPOS_DIR_PATH.length())
                 : repoFullPath;
@@ -93,59 +69,5 @@ public class ReposDir {
         }
 
         return Arrays.stream(items).anyMatch(file -> file.getName().equals(".git"));
-    }
-
-    private static class Node {
-        private final int depth;
-        private final String name;
-        private final List<Node> leaves;
-
-        private Node(int depth, String name) {
-            this.depth = depth;
-            this.name = name;
-            this.leaves = new ArrayList<>();
-        }
-
-        public void add(String repoPath) {
-            String[] pathParts = repoPath.split("/");
-            add(pathParts, 0);
-        }
-
-        private void add(String[] repoPathParts, int index) {
-            if (index >= repoPathParts.length) {
-                return;
-            }
-
-            String part = repoPathParts[index];
-            var node = this.leaves.stream().filter(leaf -> leaf.name.equals(part)).findFirst();
-
-            if (node.isPresent()) {
-                node.get().add(repoPathParts, index + 1);
-            } else {
-                var newNode = new Node(this.depth + 1, part);
-
-                this.leaves.add(newNode);
-
-                newNode.add(repoPathParts, index + 1);
-            }
-        }
-
-        public void asTree(StringBuilder builder, String prefix, boolean isLast) {
-            if (depth >= 0) {
-                builder.append(prefix)
-                        .append(isLast ? "└─ " : "├─ ")
-                        .append(name)
-                        .append("\n");
-            }
-
-            for (int i = 0; i < leaves.size(); i++) {
-                boolean lastChild = i == leaves.size() - 1;
-                leaves.get(i).asTree(
-                        builder,
-                        prefix + (isLast ? "   " : "│  "),
-                        lastChild
-                );
-            }
-        }
     }
 }
--- app/src/main/java/se/augustocesar/repos/commands/ListCommand.java
+++ app/src/main/java/se/augustocesar/repos/commands/ListCommand.java
@@ -36,7 +36,7 @@ public class ListCommand implements Command {
 
     @Override
     public Integer call() {
-        String output = ReposDir.load().displayTree(this.args.filter);
+        String output = this.displayTree(this.args.filter);
 
         System.out.println(output);
 
@@ -53,5 +53,85 @@ public class ListCommand implements Command {
                 """;
 
         return Optional.of(help);
+    }
+
+    public String displayTree(String filter) {
+        ReposDir reposDir = ReposDir.load();
+
+        var filteredRepos = reposDir.list(filter).stream()
+                .map(repo -> {
+                    String path = ReposDir.pathFromBase(repo);
+
+                    return path.startsWith("/") ? path.substring(1) : path;
+                })
+                .sorted()
+                .toList();
+
+        Node root = new Node(-1, "root");
+        for (String repoPath : filteredRepos) {
+            root.add(repoPath);
+        }
+
+        StringBuilder tree = new StringBuilder();
+        for (int i = 0; i < root.leaves.size(); i++) {
+            boolean isLast = i == root.leaves.size() - 1;
+            root.leaves.get(i).asTree(tree, "", isLast);
+        }
+
+        return tree.toString();
+    }
+
+    private static class Node {
+        private final int depth;
+        private final String name;
+        private final List<Node> leaves;
+
+        private Node(int depth, String name) {
+            this.depth = depth;
+            this.name = name;
+            this.leaves = new ArrayList<>();
+        }
+
+        public void add(String repoPath) {
+            String[] pathParts = repoPath.split("/");
+            add(pathParts, 0);
+        }
+
+        private void add(String[] repoPathParts, int index) {
+            if (index >= repoPathParts.length) {
+                return;
+            }
+
+            String part = repoPathParts[index];
+            var node = this.leaves.stream().filter(leaf -> leaf.name.equals(part)).findFirst();
+
+            if (node.isPresent()) {
+                node.get().add(repoPathParts, index + 1);
+            } else {
+                var newNode = new Node(this.depth + 1, part);
+
+                this.leaves.add(newNode);
+
+                newNode.add(repoPathParts, index + 1);
+            }
+        }
+
+        public void asTree(StringBuilder builder, String prefix, boolean isLast) {
+            if (depth >= 0) {
+                builder.append(prefix)
+                        .append(isLast ? "└─ " : "├─ ")
+                        .append(name)
+                        .append("\n");
+            }
+
+            for (int i = 0; i < leaves.size(); i++) {
+                boolean lastChild = i == leaves.size() - 1;
+                leaves.get(i).asTree(
+                        builder,
+                        prefix + (isLast ? "   " : "│  "),
+                        lastChild
+                );
+            }
+        }
     }
 }
