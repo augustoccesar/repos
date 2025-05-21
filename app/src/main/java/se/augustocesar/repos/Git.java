0a53dcc87ec27db3b98fba5ed97f86b1d75bf261
Augusto César <augusto@augustocesar.se>
2025-05-21 11:39:58 -0300

    feat: add `track` command
    This commands makes so that you can move repositories that are on other places
    under the repos structure.

--- app/src/main/java/se/augustocesar/repos/Git.java
+++ app/src/main/java/se/augustocesar/repos/Git.java
@@ -1,8 +1,14 @@
 package se.augustocesar.repos;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Optional;
+import java.util.logging.Logger;
 
 public class Git {
+    private static final Logger logger = Logger.getLogger(Git.class.getName());
+
     public static boolean clone(final String url, final String target) {
         try {
             var processBuilder = new ProcessBuilder("git", "clone", url, target).inheritIO();
@@ -17,6 +23,32 @@ public class Git {
             System.err.println("Interrupted cloning.");
 
             return false;
+        }
+    }
+
+    public static Optional<String> getRemote() {
+        try {
+            var processBuilder = new ProcessBuilder("git", "remote", "get-url", "origin");
+            Process process = processBuilder.start();
+
+            var output = new StringBuilder();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    output.append(line);
+                }
+            }
+
+            var outputStr = output.toString();
+            if (process.waitFor() == 0 && !outputStr.isBlank()) {
+                return Optional.of(output.toString());
+            } else {
+                return Optional.empty();
+            }
+        } catch (IOException | InterruptedException e) {
+            logger.severe("Failed to resolve the repository remote: " + e);
+
+            return Optional.empty();
         }
     }
 }
