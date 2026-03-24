ff80250085898db6fdb0786ab9eab9cfa18e9c03
Augusto César <augusto@augustocesar.se>
2026-03-24 08:18:00 +0100

    feat: support cloning missing repository on expand

--- src/commands/expand.rs
+++ src/commands/expand.rs
@@ -1,4 +1,10 @@
+use std::{
+    io::{self, Write},
+    path::PathBuf,
+};
+
 use anyhow::{Context, anyhow};
+use gix_url::Url;
 
 use crate::{config::Config, git};
 
@@ -19,11 +25,58 @@ The index of a repository can be checked on the config.toml file or by running `
 
 pub async fn handle(args: &Args, config: &Config) {
     match expand(args.name.as_deref(), config) {
-        Ok(path) => {
-            println!("{}", path);
+        Ok(repository) => match (repository.path.exists(), args.clone) {
+            (true, _) => {
+                println!("{}", repository.path.to_string_lossy());
+                std::process::exit(0);
+            }
+            (false, false) => {
+                println!(
+                    "Repository not found!\nLookup path: {}",
+                    repository.path.to_string_lossy()
+                );
+                std::process::exit(1);
+            }
+            (false, true) => {
+                println!("Repository not found locally.");
+                println!(
+                    "Lookup path: {}",
+                    repository
+                        .path
+                        .to_str()
+                        .expect("constructed local path should be valid unicode")
+                );
+                println!(
+                    "Do you want to clone it from {}? (y, N)",
+                    repository.clone_url.to_bstring()
+                );
 
-            std::process::exit(0);
-        }
+                loop {
+                    let mut answer = String::new();
+                    let _ = io::stdout().flush();
+                    io::stdin()
+                        .read_line(&mut answer)
+                        .expect("Input should be a valid string");
+
+                    match answer.trim().to_lowercase().as_str() {
+                        "y" => break,
+                        "n" => {
+                            println!("Aborted!");
+                            std::process::exit(1);
+                        }
+                        _ => println!("Invalid input"),
+                    }
+                }
+
+                if let Err(error) = git::clone(&repository.clone_url, &repository.path) {
+                    println!("Failed to clone repository: {error}");
+                    std::process::exit(1);
+                }
+
+                println!("{}", repository.path.to_string_lossy());
+                std::process::exit(0);
+            }
+        },
         Err(error) => {
             eprintln!("{}", error);
 
@@ -32,7 +85,7 @@ pub async fn handle(args: &Args, config: &Config) {
     }
 }
 
-fn expand(input: Option<&str>, config: &Config) -> Result<String, anyhow::Error> {
+fn expand(input: Option<&str>, config: &Config) -> Result<Repository, anyhow::Error> {
     let base_path = &config.base_path;
 
     let input = match input {
@@ -52,92 +105,108 @@ fn expand(input: Option<&str>, config: &Config) -> Result<String, anyhow::Error>
         }
     };
 
-    let input = input.trim_start_matches('/');
-    let url = gix_url::parse(input.into()).context("parsing repository name arg")?;
+    Repository::resolve(input, config)
+}
 
-    match url.scheme {
-        gix_url::Scheme::File => {
-            if let Some(index) = &config.index
-                && url.path.starts_with(b"@")
-            {
-                let key = std::str::from_utf8(&url.path[1..])
-                    .context("failed to get index key from input")?;
+#[derive(Debug)]
+struct Repository {
+    path: PathBuf,
+    clone_url: Url,
+}
 
-                let path = index
-                    .get(key)
-                    .ok_or_else(|| anyhow!("index '{}' not found", key))?;
+impl Repository {
+    fn resolve(input: &str, config: &Config) -> Result<Self, anyhow::Error> {
+        let input = input.trim_start_matches('/');
+        let url = gix_url::parse(input.into()).context("parsing repository name arg")?;
 
-                let full_path = base_path.join(path).to_string_lossy().to_string();
+        match url.scheme {
+            gix_url::Scheme::File => {
+                if let Some(index) = &config.index
+                    && url.path.starts_with(b"@")
+                {
+                    let key = std::str::from_utf8(&url.path[1..])
+                        .context("failed to get index key from input")?;
 
-                return Ok(full_path);
+                    let path = index
+                        .get(key)
+                        .ok_or_else(|| anyhow!("index '{}' not found", key))?;
+
+                    return Self::resolve(path, config);
+                }
+
+                if let Some(aliases) = &config.aliases
+                    && !url.path.contains(&b'/')
+                {
+                    if let Some(input) = aliases.get(&url.path.to_string()) {
+                        return Self::resolve(input, config);
+                    }
+                }
+
+                let path = url.path.to_string();
+                let path_parts = path.split("/").collect::<Vec<&str>>();
+
+                let (host, user, repo) = match path_parts.len() {
+                    1 => {
+                        // This will assume that the value is the repository name and will use the default
+                        // host and default user to create a {default_host}/{default_user}/{repository} format.
+                        (
+                            config.host.as_str(),
+                            config.username.as_str(),
+                            path_parts[0],
+                        )
+                    }
+                    2 => {
+                        // This will assume that we have both the user and repository, and will use the default
+                        // host to create a {default_host}/{user}/{repository} format.
+                        (config.host.as_str(), path_parts[0], path_parts[1])
+                    }
+                    3 => {
+                        // This will assume that we have the host, user and repository, and will
+                        // create a {host}/{user}/{repository} format.
+                        (path_parts[0], path_parts[1], path_parts[2])
+                    }
+                    _ => {
+                        // TODO: Not sure about this one.
+                        todo!()
+                    }
+                };
+
+                let full_path = config.base_path.join(host).join(user).join(repo);
+                let clone_url = Url::from_parts(
+                    gix_url::Scheme::Ssh,
+                    Some(user.to_owned()),
+                    None,
+                    Some(host.to_owned()),
+                    None,
+                    format!("/{repo}").into(),
+                    false,
+                )?;
+
+                return Ok(Self {
+                    path: full_path,
+                    clone_url,
+                });
             }
+            gix_url::Scheme::Https
+            | gix_url::Scheme::Http
+            | gix_url::Scheme::Ssh
+            | gix_url::Scheme::Git => {
+                let path = url.path.to_string();
+                let path = path.trim_start_matches('/').trim_end_matches(".git");
 
-            if let Some(aliases) = &config.aliases
-                && !url.path.contains(&b'/')
-            {
-                if let Some(input) = aliases.get(&url.path.to_string()) {
-                    return expand(Some(input), config);
-                }
+                let host = url
+                    .host()
+                    .ok_or_else(|| anyhow!("Expects git urls to have a host"))?;
+
+                let full_path = config.base_path.join(host).join(path);
+
+                return Ok(Self {
+                    path: full_path,
+                    clone_url: url,
+                });
             }
-
-            let path = url.path.to_string();
-            let path_parts = path.split("/").collect::<Vec<&str>>();
-
-            let (host, user, repo) = match path_parts.len() {
-                1 => {
-                    // This will assume that the value is the repository name and will use the default
-                    // host and default user to create a {default_host}/{default_user}/{repository} format.
-                    (
-                        config.host.as_str(),
-                        config.username.as_str(),
-                        path_parts[0],
-                    )
-                }
-                2 => {
-                    // This will assume that we have both the user and repository, and will use the default
-                    // host to create a {default_host}/{user}/{repository} format.
-                    (config.host.as_str(), path_parts[0], path_parts[1])
-                }
-                3 => {
-                    // This will assume that we have the host, user and repository, and will
-                    // create a {host}/{user}/{repository} format.
-                    (path_parts[0], path_parts[1], path_parts[2])
-                }
-                _ => {
-                    // TODO: Not sure about this one.
-                    return Ok(path);
-                }
-            };
-
-            let full_path = base_path
-                .join(host)
-                .join(user)
-                .join(repo)
-                .to_string_lossy()
-                .to_string();
-
-            return Ok(full_path);
+            gix_url::Scheme::Ext(_) => return Err(anyhow!("Unsupported URL scheme")),
         }
-        gix_url::Scheme::Https
-        | gix_url::Scheme::Http
-        | gix_url::Scheme::Ssh
-        | gix_url::Scheme::Git => {
-            let path = url.path.to_string();
-            let path = path.trim_start_matches('/').trim_end_matches(".git");
-
-            let host = url
-                .host()
-                .ok_or_else(|| anyhow!("Expects git urls to have a host"))?;
-
-            let full_path = base_path
-                .join(host)
-                .join(path)
-                .to_string_lossy()
-                .to_string();
-
-            return Ok(full_path);
-        }
-        gix_url::Scheme::Ext(_) => return Err(anyhow!("Unsupported URL scheme")),
     }
 }
 
@@ -208,7 +277,7 @@ mod test {
             );
 
             assert_eq!(
-                result.unwrap(),
+                result.unwrap().path.to_str().unwrap(),
                 expected_path,
                 "expand('{}') returned incorrect path",
                 example
--- src/git.rs
+++ src/git.rs
@@ -1,4 +1,7 @@
+use std::path::Path;
+
 use anyhow::{Context, anyhow};
+use gix_url::Url;
 
 pub fn get_repo_root() -> Result<String, anyhow::Error> {
     let rev_parse_output = std::process::Command::new("git")
@@ -12,6 +15,22 @@ pub fn get_repo_root() -> Result<String, anyhow::Error> {
     } else {
         let error = String::from_utf8(rev_parse_output.stderr)
             .expect("git rev-parse stderr should be valid utf8");
+
+        Err(anyhow!(error))
+    }
+}
+
+pub fn clone(url: &Url, destination: &Path) -> Result<(), anyhow::Error> {
+    let clone_output = std::process::Command::new("git")
+        .args(&["clone", &url.to_string(), &destination.to_string_lossy()])
+        .output()
+        .context("failed to execute git clone")?;
+
+    if clone_output.status.success() {
+        Ok(())
+    } else {
+        let error =
+            String::from_utf8(clone_output.stderr).expect("git clone stderr should be valid utf8");
 
         Err(anyhow!(error))
     }
